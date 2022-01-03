7d265c30d60f70765cfe600990e3d7d4475f142d
DanielNagy97 <nagydaniel.z97@gmail.com>
2022-01-03 13:06:42 +0100

    onBackPressed logic

--- app/src/main/java/hu/unimiskolc/iit/mobile/untitledwestern/application/MainActivity.kt
+++ app/src/main/java/hu/unimiskolc/iit/mobile/untitledwestern/application/MainActivity.kt
@@ -2,11 +2,22 @@ package hu.unimiskolc.iit.mobile.untitledwestern.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.unimiskolc.iit.mobile.untitledwestern.application.fragment.EndGameFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        val count = supportFragmentManager.backStackEntryCount
+        if (count == 0) {
+            super.onBackPressed()
+            this.finish()
+        } else {
+            supportFragmentManager.popBackStack()
+        }
     }
 }
 
